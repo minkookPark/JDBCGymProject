99ecf7c1d533b413895a5ffbc6c8cc2818f01937
apzkxkdn <impsim@naver.com>
2024-09-09 13:00:58 +0900

    trainderdao 추가

--- src/민국/TestMain.java
+++ src/민국/TestMain.java
@@ -3,8 +3,26 @@ package 민국;
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("gd");
+
+		JDBCTrainerDao tDao = new JDBCTrainerDao();
+
+//		Trainer testInserTrainer = new Trainer("qwer","a1234","남자","있어용",27);
+//
+//		tDao.insert(testInserTrainer);
+//
+//		for(Trainer t : tDao.findAll())
+//		{
+//			System.out.println(t);
+//		}
+
+		Trainer updateTest = new Trainer(2,"asdf","1q2w3e","여자","짱큰대회",33);
+
+		tDao.update(updateTest);
+
+
+
+		//tDao.deleteById(1);
+
 	}
 
 }
